f9c7e662eb444b2f6eb0623d68f1643f48bc8c7e
Marek Cwolek <marek.cwolek@meplato.de>
2023-02-28 15:26:46 +0100

    Add webridge styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,12 +2,17 @@ module.exports = {
 	content: ['index.html', './src/**/*.{js,jsx,ts,tsx,vue,html}'],
 	theme: {
 		extend: {
+			borderWidth: { 3: '3px' },
 			colors: {
 				webridge: {
+					error: '#D91515',
+					'error-border': '#970F0F',
 					'dark-gray': '#2D2D2D',
 					'light-gray': '#F2F2F2',
+					green: '#3CBE14',
 				},
 			},
+			opacity: { 35: '0.35' },
 		},
 	},
 	plugins: [],

510acd29e5030feab55f35aea9561cec25c1fd56
Marek Cwolek <marek.cwolek@meplato.de>
2023-02-28 12:31:56 +0100

    Init app

---
+++ src/App.tsx
@@ -0,0 +1,5 @@
+const App = () => {
+	return <div className="p-4"></div>;
+};
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+	content: ['index.html', './src/**/*.{js,jsx,ts,tsx,vue,html}'],
+	theme: {
+		extend: {
+			colors: {
+				webridge: {
+					'dark-gray': '#2D2D2D',
+					'light-gray': '#F2F2F2',
+				},
+			},
+		},
+	},
+	plugins: [],
+};
